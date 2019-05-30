3966eb65c12a1fd72a2e7b0793d37b81920d283e
Ed Santos <esantos@live.ca>
2019-05-30 19:20:11 -0400

    Initial Commit

---
+++ NavViewControllerTests/NavViewControllerTests.swift
@@ -0,0 +1,34 @@
+//
+//  NavViewControllerTests.swift
+//  NavViewControllerTests
+//
+//  Created by Ed Santos on 2019-05-30.
+//  Copyright © 2019 Ed Santos. All rights reserved.
+//
+
+import XCTest
+@testable import NavViewController
+
+class NavViewControllerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
